e353596da1485b5dbb780c8afe70f2ee5f3c78a0
muhammadmasih <Muhammedmasih333@gmail.com>
2025-01-08 13:53:50 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function headerShadow() {
 }
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Founder","CEO"],
+  strings : ["Founder&President","Bazan Institute"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
